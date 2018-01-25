0818ae89703e97fa8d0902005eca5dd56e64e7ed
rchaser53 <tayoshizawa29@gmail.com>
2018-01-25 20:54:38 +0900

    fix: ignore empty last line

--- src/ts/__tests__/converter.ts
+++ src/ts/__tests__/converter.ts
@@ -1,21 +1,26 @@
-import { converter } from '../converter'
+import { converter } from "../converter";
 
-describe('converter', () => {
-	test('convert values having new line to array in array', async () => {
-		const input = `1	2	3
-4	5	6`
-		expect(converter(input)).toEqual([
-      '1', '2', '3', '4', '5', '6'
-    ])
-  })
-  
-	test('convert values having new line to empty array', async () => {
-		const input = ``
-		expect(converter(input)).toEqual([])
-  })
-  
-	test('convert single values to array having single value', async () => {
-		const input = `1`
-		expect(converter(input)).toEqual(['1'])
-  })
-})
+describe("converter", () => {
+  test("convert values having new line to array in array", async () => {
+    const input = `1	2	3
+4	5	6`;
+    expect(converter(input)).toEqual(["1", "2", "3", "4", "5", "6"]);
+  });
+
+  test("convert values having new line to empty array", async () => {
+    const input = ``;
+    expect(converter(input)).toEqual([]);
+  });
+
+  test("convert single values to array having single value", async () => {
+    const input = `1`;
+    expect(converter(input)).toEqual(["1"]);
+  });
+
+  test("ignore last line empty value", async () => {
+    const input = `1
+		`;
+    expect(converter(input)).toEqual(["1"]);
+  });
+
+});
--- src/ts/converter.ts
+++ src/ts/converter.ts
@@ -1,10 +1,23 @@
 export const converter = (value: string): string[] => {
-  const rows = value.split('\n');
+  const rows = value.split("\n");
 
-  return rows.reduce<string[]>((stack, next) => {
-    return next === ''
-            ? stack
-            : stack.concat(next.split('\t'))
+  const retValue = rows.reduce<string[]>((stack, next) => {
+    return next === ""
+      ? stack
+      : stack.concat(next.split("\t").reduce(removeEmptyReduce, []));
   }, []);
-  
-}
+
+  return trimLastLine(retValue);
+};
+
+const removeEmptyReduce = (stack: string[], next: string): string[] => {
+  return next === "" ? stack : stack.concat([next]);
+};
+
+export const trimLastLine = (rows: string[]): string[] => {
+  if (rows.length <= 1) return rows;
+  const lastElement = rows.pop() as string;
+  return lastElement.match(/^\s*$/) === null
+    ? rows.concat([lastElement])
+    : rows;
+};
